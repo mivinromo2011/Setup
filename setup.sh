6cf08c33a4a2bb5fe75deee06e773758ab3f0089
mivinromo2011 <mithun.rosinth@gmail.com>
2020-05-24 20:30:17 +0530

    setup script

---
+++ setup.sh
@@ -0,0 +1,32 @@
+apt update -y
+apt upgrade -y
+
+apt install terminator -y
+apt install chromium -y
+apt install sanpd -y
+apt install terminator -y
+apt install flameshot -y
+apt install wireshark -y
+apt install virtualbox -y
+apt install telegram-desktop -y
+apt install chrome-gnome-shell -y
+apt install gnome-tweaks -y
+apt install plank -y
+apt install htop -y
+apt install nmap -y
+apt install evolution -y
+apt install vim -y
+snap install spotify
+snap install code --classic
+snap install discord
+apt install uget
+
+add-apt-repository ppa:uget-team/ppa
+apt update
+apt install uget-integrator
+
+add-apt-repository ppa:papirus/papirus
+apt-get update
+apt-get install papirus-icon-theme
+
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
