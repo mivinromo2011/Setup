6a2b44de8ea7f9e7fe2343d1f264ebf04b2e7049
Mithun <mithun.rosinth2011@protonmail.com>
2020-06-17 16:08:51 +0530

    Changed setup files and added files-fetch

---
+++ files-fetch.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install git -y
+
+git clone https://github.com/mivinromo2011/CTFs.git
+git clone https://github.com/mivinromo2011/Setup.git
+git clone https://github.com/mivinromo2011/Courses.git
--- packages.sh
+++ packages.sh
@@ -5,7 +5,6 @@ apt install git -y
 apt install wget -y
 apt install flameshot -y
 apt install wireshark -y
-apt install virtualbox -y
 apt install telegram-desktop -y
 apt install htop -y
 apt install vim -y
@@ -16,4 +15,9 @@ add-apt-repository ppa:uget-team/ppa
 apt update
 apt install uget-integrator -y
 apt install apt-transport-https curl
-
+apt install ffmpeg -y
+add-apt-repository ppa:obsproject/obs-studio
+apt install obs-studio -y
+add-apt-repository ppa:appimagelauncher-team/stable
+apt install appimagelauncher -y
+apt install docker.io -y
