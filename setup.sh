756f9528edb7c3c056d829d2b7907a258a38a951
mivinromo2011 <mithun.rosinth2011@protonmail.com>
2021-04-02 09:39:03 +0530

    backup

--- setup.sh
+++ setup.sh
@@ -8,17 +8,11 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
 #configuration
 rm /home/mivin/.zshrc &&
-ln -s /home/mivin/dotfiles/terminator /home/mivin/.config/terminator &&
-ln -s /home/mivin/dotfiles/flameshot /home/mivin/.config/flameshot &&
-ln -s /home/mivin/dotfiles/.zshrc /home/mivin/.zshrc &&
-ln -s /home/mivin/dotfiles/sublime-text-3/Packages /home/mivin/.config/sublime-text-3/ &&
+ln -sf /home/mivin/dotfiles/.zshrc /home/mivin/.zshrc &&
+ln -sf /home/mivin/dotfiles/.tmux.conf /home/mivin/.tmux.conf &&
 #aur
 git clone https://aur.archlinux.org/paru.git &&
 cd paru && makepkg -si &&
 #git-config
 git config --global user.name "mivinromo2011" &&
 git config --global user.email "mithun.rosinth2011@protonmail.com" &&
-#Wallpapers
-sudo cp /home/mivin/Setup/Wallpapers/* /usr/share/backgrounds &&
-#touchpad
-sudo ln -s /home/mivin/dotfiles/30-touchpad.conf /etc/X11/xorg.conf.d &&
